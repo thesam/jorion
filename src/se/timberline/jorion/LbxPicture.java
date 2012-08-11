7da944ac468d5539f18eff10ae49b646212895b1
Samuel Moritz <samuel.moritz@gmail.com>
2012-08-12 01:02:33 +0200

    First four columns of the "Human" image display correctly.

--- src/se/timberline/jorion/LbxPicture.java
+++ src/se/timberline/jorion/LbxPicture.java
@@ -32,39 +32,62 @@ public class LbxPicture {
 	}
 
 	public void draw(Graphics g) {
-		blob.seek(0x1F);
+		// blob.seek(0x1F);
+		blob.seek(0x1B);
 		int width2 = getWidth();
 		int height2 = getHeight();
 		int nextByte = -1;
 		int currentX = 0;
 		int currentY = 0;
 		System.err.println("Drawing " + width2 + " x " + height2);
+		boolean lineModeEnabled = true;
+		currentX = -1;
 		do {
-
+			currentY = 0;
+			currentX++;
 			nextByte = blob.readUInt8();
-			if (nextByte != -1) {
-				int pixelCounter = 1;
-				if (nextByte >= 0xE0 && nextByte <= 0xEF) { // TODO: Find out upper limit, can it be related to the 4 skipped bytes?
-					pixelCounter += nextByte & 0x0F;
-					nextByte = blob.readUInt8();
-				}
-				while (pixelCounter > 0) {
-					Color color = getColorFromPalette(nextByte);
-					g.setColor(color);
-//					System.err.println("Draw to " + currentX + "-" + currentY);
-					g.drawLine(currentX, currentY, currentX, currentY);
-					currentY++;
-					pixelCounter--;
+			if (nextByte == 0x00) {
+				lineModeEnabled = false;
+			} else {
+				lineModeEnabled = true;
+			}
+			nextByte = blob.readUInt8();
+			nextByte = blob.readUInt8();
+			nextByte = blob.readUInt8();
+			while (currentY < getHeight() && nextByte != -1) {
+				nextByte = blob.readUInt8();
+				if (nextByte != -1) {
+					int pixelCounter = 1;
+					if ((nextByte >= 0xE0 && nextByte <= 0xEF)
+							&& lineModeEnabled) { // TODO:
+													// Find
+													// out
+													// upper
+													// limit,
+													// can
+													// it
+													// be
+													// related
+													// to
+													// the
+													// 4
+													// skipped
+													// bytes?
+						pixelCounter += nextByte & 0x0F;
+						nextByte = blob.readUInt8();
+					}
+					while (pixelCounter > 0) {
+						Color color = getColorFromPalette(nextByte);
+						g.setColor(color);
+						// System.err.println("Draw to " + currentX + "-" +
+						// currentY);
+						g.drawLine(currentX, currentY, currentX, currentY);
+						currentY++;
+						pixelCounter--;
+					}
 				}
 			}
-			if (currentY >= getHeight()) {
-				currentY = 0;
-				currentX++;
-				nextByte = blob.readUInt8();
-				nextByte = blob.readUInt8();
-				nextByte = blob.readUInt8();
-				nextByte = blob.readUInt8();
-			}
+
 		} while (nextByte != -1);
 
 		// g.drawRect(0, 0, width2, height2);
@@ -72,14 +95,16 @@ public class LbxPicture {
 
 	private Color getColorFromPalette(int nextByte) {
 		Color color = null;
-		Map<Integer,Color> palette = new HashMap<Integer,Color>();
-		
-		palette.put(0x20,new Color(32, 48, 81));
-		palette.put(0x28,new Color(36, 56, 101));
-		palette.put(0x30,new Color(36, 60, 125));
-		palette.put(0xBF,new Color(97, 73, 0));
-		palette.put(0xC0,new Color(150, 113, 0));
-		
+		Map<Integer, Color> palette = new HashMap<Integer, Color>();
+
+		palette.put(0x20, new Color(32, 48, 81));
+		palette.put(0x28, new Color(36, 56, 101));
+		palette.put(0x30, new Color(36, 60, 125));
+		palette.put(0xBF, new Color(97, 73, 0));
+		palette.put(0xC0, new Color(150, 113, 0));
+		palette.put(0xC1, new Color(203, 150, 0));
+		palette.put(0xED, new Color(170, 121, 77));
+
 		if (palette.containsKey(nextByte)) {
 			color = palette.get(nextByte);
 		} else {
